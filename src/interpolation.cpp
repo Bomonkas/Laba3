fcebaf0bdcaae1752dcb3a562a30fe19b5343bff
vboled <56510659+vboled@users.noreply.github.com>
2019-11-23 21:13:09 +0300

    i'm in the out

--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -1,25 +1,26 @@
 #include "interpolation.h"
 
-double   *get_uniform_grid()
+double   *get_uniform_grid(double a, double b, int n)
 {
-    int     n = NUM;
-    double *grid = new double[n];
-    int     i = 0;
+    double *grid = new double[n + 1];
+    double  x;
 
-    while (n-- >= 0)
+    for (int i = 0; i <= n; i++)
     {
-        grid[i] = LEFT + (LEFT - RIGHT) / NUM * i;
-        i++;
+        x = a + (b - a) / n * i;
+        cout << "x = " << x << " FUNC(x) = " << FUNC(x) << endl;
+        grid[i] = FUNC(x);
+        n--;
     }
     return (grid);
 }
 
-void      print_uniform_grid(double *grid)
+void      print_uniform_grid(double *grid, double a, double b, int n)
 {
-    for (int i = 0; i < NUM; i++)
-        cout << setw(3) <<  LEFT + (LEFT - RIGHT) / NUM * i << " ";
+    for (int i = 0; i <= n; i++)
+        cout << setw(6) <<  a + (b - a) / n * i << " | ";
     cout << endl;
-    for (int i = 0; i < NUM; i++)
-        cout << setw(3) <<  grid[i];
+    for (int i = 0; i <= n; i++)
+        cout << setw(6) << setprecision(4) <<  grid[i] << " | ";
     cout << endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 
 int     main()
 {
-    double *uniform_grid = get_uniform_grid();
-    print_uniform_grid(uniform_grid);
+    double  a = LEFT;
+    double  b = RIGHT;
+    int     n = NUM;
+
+    double *uniform_grid = get_uniform_grid(a, b, n);
+    print_uniform_grid(uniform_grid, a, b, n);
     cout << FUNC(4) << endl;
     return (0);
 }
