2942619dde325897951b5a2668cb3697c96a2697
vboled <56510659+vboled@users.noreply.github.com>
2019-11-23 21:18:48 +0300

    uniform grid

--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -8,18 +8,17 @@ double   *get_uniform_grid(double a, double b, int n)
     for (int i = 0; i <= n; i++)
     {
         x = a + (b - a) / n * i;
-        cout << "x = " << x << " FUNC(x) = " << FUNC(x) << endl;
         grid[i] = FUNC(x);
-        n--;
     }
     return (grid);
 }
 
 void      print_uniform_grid(double *grid, double a, double b, int n)
 {
+    cout <<"uniform grid :\n" << " x =";
     for (int i = 0; i <= n; i++)
-        cout << setw(6) <<  a + (b - a) / n * i << " | ";
-    cout << endl;
+        cout << setw(6) <<  a + (b - a) / n * i<< " | ";
+    cout << endl << " y =";
     for (int i = 0; i <= n; i++)
         cout << setw(6) << setprecision(4) <<  grid[i] << " | ";
     cout << endl;
